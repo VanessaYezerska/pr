cb94cc1afe091cf9563eb62ec54cfbbfe154b02f
Vanessa Yezerska <vanessa.yezerska05@gmail.com>
2024-02-09 18:54:09 +0200

    added new methods

--- src/tasks/App.java
+++ src/tasks/App.java
@@ -1,2 +1,17 @@
-package tasks;public class App {
+package tasks;
+
+public class App {
+    int s;
+    int r;
+
+    public App(int s, int r) {
+        this.s = s;
+        this.r = r;
+    }
+    int add(){
+        return s + r;
+    }
+    void minus(){
+        System.out.println(s-r);
+    }
 }
---
+++ src/tasks/Main.java
@@ -0,0 +1,11 @@
+package tasks;
+
+public class Main {
+    public static void main(String[] args) {
+        App app = new App(23,22);
+        System.out.println(app.add());
+        app.minus();
+
+
+    }
+}
